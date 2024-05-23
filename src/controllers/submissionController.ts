b983156e43ebb1f652ed949271a6c60268cf51b3
Nishant <nishantdongre30@gmail.com>
2024-05-23 14:34:46 +0530

    Feat: Added validation layer using zod

---
+++ src/controllers/submissionController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { CreateSubmissionDto } from '../dtos/CreateSubmissionDto';
+
+export function addSubmission(req: Request, res: Response) {
+    const submissionDto = req.body as CreateSubmissionDto;
+    console.log(submissionDto);
+
+    return res.status(StatusCodes.CREATED).json({
+        success: true,
+        error: {},
+        message: 'Successfully collected the submission',
+        data: submissionDto
+    });
+}
